e13dfa0e3d49c26d6a9bc128f65c4c1847e26992
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-10 13:18:22 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,5 @@
 sudo yum install -y git 
-sudo git clone https://github.com/GOUSERABBANI44/JavaCalculator.git
+sudo git clone https://github.com/GOUSERABBANI44/hello-world.git
 cd JavaCalculator
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
