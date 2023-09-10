c3f81e90a969d01f62168f527e16fc141d127e99
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-10 13:00:44 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,8 @@
+sudo yum install -y git 
+sudo git clone https://github.com/GOUSERABBANI44/JavaCalculator.git
+cd JavaCalculator
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn –version
+mvn clean package
