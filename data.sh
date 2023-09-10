6e3c729f25abcda9c9ad2ac89ecba23a56edc078
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-10 14:37:40 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,6 @@
 sudo yum install -y git 
 sudo yum install -y java-1.8*
 sudo git clone https://github.com/anilkumar3577/hello-world-1.git
-cd /home/ec2-user/workspace/java2/hello-world-1/webapp
+cd hello-world-1/webapp
 sudo yum install -y maven
-mvn –version
 mvn package
