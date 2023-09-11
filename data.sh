49d8e77b0fa834456bda1e0a3807c5220b818eb1
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-11 12:29:43 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,3 @@
-sudo git clone https://github.com/anilkumar3577/maven-web-app.git
+sudo git clone https://github.com/ajay77777777/maven-web-app.git
 cd /home/ec2-user/workspace/java2/maven-web-app/target
 sudo cp -r 01-maven-web-app.war /home/ec2-user/workspace/tomcat/apache-tomcat-8.5.93/webapps
